a815e37ef634e6406d82eb93c04df689957f57ef
leuchtend5 <zxcmj1@gmail.com>
2022-03-10 23:52:54 +0800

    update

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const inputError = document.getElementsByClassName("error");
+const inputPassword = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+const errorPwc = document.getElementById("error-pwc");
+const errorTel = document.getElementById("error-tel");
+const errorPw = document.getElementById("error-pw");
+const span = document.getElementsByTagName("span");
+
+function passwordValidate() {
+  if (confirmPassword.value !== inputPassword.value) {
+    errorPwc.textContent = "Password do not match";
+  } else {
+    errorPwc.textContent = "";
+  }
+}
+
+confirmPassword.addEventListener("input", passwordValidate);
+
+function requirementValidate() {}
